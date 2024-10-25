1280704d4aa4fff5c9ca2f92f58d35ae1b4ff401
Kpure1000 <ktsq2000@126.com>
2024-10-25 18:01:15 +0800

    add: some dataset

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
-# 数据集
+# Quantitative Evaluation of Dimensionality Reduction Techniques for Out-of-Sample Data
 
-## 真实数据集
+[code](https://github.com/Kpure1000/OOS-DR-Benchmark)
 
+## Datasets
 
+### Synthetic
+
+1. Coordinated Plane
+
+2. Swissroll Cut by Plane
+
+3. Digit 8 
+
+TBC...
+
+### Real world
+
+Scenario 1: Visual Cluster Analysis
+
+7 dataset with class label
+
+|name|type|N_samples|N_dims|classes|
+|-|-|-|-|-|
+|letter|images|6450|512|?|
+|fashionMNIST|images|4200|784|?|
+|fmd|images|997|?|?|
+|secom|tables|1567|?|?|
+|cnae9|text|1080|856|9|
+|spambase|text|4601|57|2|
+|hatespeech|text|2973|?|2|
+
+Scenario 2: Distribution Diff 
+
+6 dataset with distribution difference
+
+|name|type|N_samples|N_dims|classes|KLdiv|
+|-|-|-|-|-|-|
+|raid|images|1955|?|?|?|
+|sensor|tables|7213|?|?|?|
+|dendritic|tables|576|?|?|?|
+|pancrease|tables|7383|?|?|?|
+|retina|tables|7606|?|?|?|
+|merchant|text|3897|?|?|?|
+
+#### Scenario 3: Intrinsic Structure Analysis
+
+4 dataset with obvious manifold structure
+
+|name|type|N_samples|N_dims|
+|-|-|-|-|
+|buffalo|tables|?|3|
+|isomapFace|images|698|512|
+|freyFace|images|1965|560|
+|coil20|images|1440|?|
--- utils.py
+++ utils.py
@@ -16,6 +16,30 @@ def load_projection(path):
     y = df['y'].to_numpy()
     return np.concatenate((x[:, np.newaxis], y[:, np.newaxis]), axis=1)
 
+def save_raw_data(path, X, **kwargs):
+    '''
+    Save raw data to hdf5 file
+    
+    Parameters
+    ----------
+    path: str
+        path to save file
+    X: np.ndarray
+        raw data
+    kwargs: dict
+        additional data to save
+
+    Example
+    -------
+    >>> save_raw_data('data.h5', X, y=labels)
+    keys: [X, y]
+    '''
+
+    with h5.File(path, 'w') as f:
+        f.create_dataset('X', data=X)
+        for k, v in kwargs.items():
+            f.create_dataset(k, data=v)
+
 def save_dataset(path, X_train, X_test, y_train, y_test):
     save_datasets(path, [(X_train, X_test, y_train, y_test)])
 
@@ -43,3 +67,42 @@ def load_datasets(path, n_stages=1):
 
 def load_dataset(path):
     return load_datasets(path)[0]
+
+def plot_projection(projects:list, titles:list=None, y_train=None, y_test=None, cmap=None, save_path=None, train_size=3, test_size=4, train_alpha=0.1, test_alpha=0.5, train_labeled=False):
+    import matplotlib.pyplot as plt
+
+    ncols = min(4, len(projects))
+    nrows = int(np.ceil(len(projects) / ncols))
+
+    titles = [f'Method {str(i+1)}' for i in range(len(projects))] if titles is None else titles
+
+    fig, ax = plt.subplots(nrows, ncols, figsize=(4 * ncols, 4 * nrows))
+    for i, (ptrain, ptest) in enumerate(projects):
+        r = i // nrows
+        c = i % ncols
+        axes = ax[r, c] if nrows > 1 else ax[i] if ncols > 1 else ax
+
+        label_train = np.zeros(ptrain.shape[0]) if y_train is None else y_train
+        label_test = np.zeros(ptest.shape[0]) if y_test is None else y_test
+
+        cmap_train = cmap if cmap is not None else 'tab10' if len(np.unique(label_train)) <= 10 else 'tab20'
+        cmap_test  = cmap if cmap is not None else 'tab10' if len(np.unique(label_test )) <= 10 else 'tab20'
+
+        axes.scatter(ptrain[:,0], ptrain[:,1],
+                     c=label_train if train_labeled else 'gray',
+                     cmap=cmap_train if train_labeled else None,
+                     s=train_size, alpha=train_alpha)
+
+        axes.scatter(ptest[:,0], ptest[:,1],
+                     c=label_test,
+                     cmap=cmap_test,
+                     s=test_size, alpha=test_alpha)
+
+        axes.set_title(titles[i])
+        axes.set_xticks([])
+        axes.set_yticks([])
+
+    fig.tight_layout()
+
+    if save_path:
+        fig.savefig(save_path, dpi=400)
